185fd28eb96947693e74b224d0f802c9e46126ce
minhhuudoan <doanminhhuu@gmail.com>
2015-11-21 11:25:33 +0700

    Thêm class DaiLyDTO

---
+++ QuanLyDaiLy/QuanLyDaiLy/DaiLyDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyDaiLy
+{
+    public class DaiLyDTO
+    {
+        private string _tenDL;
+        private string _diachi;
+        private string _sdt;
+        private string _maQuan;
+    }
+}
